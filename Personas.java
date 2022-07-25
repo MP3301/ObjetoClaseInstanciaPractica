ce3945a352a5ab85d374703323402827d9628f62
mperez30 <67341832+mperez30@users.noreply.github.com>
2022-07-25 12:08:42 -0400

    0

---
+++ Personas.java
@@ -0,0 +1,10 @@
+public class Personas {
+
+    String nombre;
+    String apellido;
+
+    public void desplegarInformacion(){
+        System.out.println("nombre = " + nombre);
+        System.out.println("apellido = " + apellido);
+    }
+}
